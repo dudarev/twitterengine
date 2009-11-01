eedeedab295714f5ed651fb82bb9af8781ce02bb
Artem Dudarev <dudarev@locograph.(none)>
2009-11-01 21:20:19 +0200

    added incoming mail support, so far mails are saved into datastore

---
+++ handle_incoming_email.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import email
+import datetime
+
+from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import webapp, db
+
+# Datamodel
+class Email(db.Model):
+    subject = db.StringProperty()
+    sender = db.EmailProperty()
+    to = db.StringListProperty()
+    cc = db.StringListProperty()
+    body = db.TextProperty()
+
+class MailHandler(InboundMailHandler):
+    def receive(self, message):
+        e = Email()
+        e.sender = message.sender
+        e.subject = message.subject
+        bodies = message.bodies(content_type='text/plain')
+        e.body = ''
+        for body in bodies:
+            e.body = e.body + body[1].decode()
+        e.put()
+
+application = webapp.WSGIApplication([ MailHandler.mapping() ], debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
